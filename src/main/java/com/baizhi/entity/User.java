440173d305ab60b31838d46fbe09a930a484e892
zhangcn <15236674712@163.com>
2019-09-24 23:24:25 +0800

    开发人员2在User实体类中又加了一个属性

--- src/main/java/com/baizhi/entity/User.java
+++ src/main/java/com/baizhi/entity/User.java
@@ -1,9 +1,12 @@
 package com.baizhi.entity;
 
+import java.util.Date;
+
 public class User {
 
     private String id;
     private String name;
     private String age;
     private String bir;
+    private Date birth;
 }
