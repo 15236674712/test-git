732454b4050c3a63bda830f9658edd344a3e0f2e
zhangcn <15236674712@163.com>
2019-09-24 23:49:34 +0800

    开发人员2在User实体类中又加了一个salary属性

---
+++ src/main/java/com/baizhi/entity/Banner.java
@@ -0,0 +1,14 @@
+package com.baizhi.entity;
+
+import java.util.Date;
+
+public class Banner {
+
+    private String id;
+    private String name;
+    private String age;
+    private String bir;
+    private Date birth;
+    private String sex;
+
+}
